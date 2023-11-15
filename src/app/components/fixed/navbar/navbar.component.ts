eed798a73db52ae5015d67d935fd497d66663314
MeDjb10 <131858818+MeDjb10@users.noreply.github.com>
2023-11-15 22:03:02 +0100

    changed the background css of home and the navbar ts

--- src/app/components/fixed/navbar/navbar.component.ts
+++ src/app/components/fixed/navbar/navbar.component.ts
@@ -9,8 +9,9 @@ export class NavbarComponent {
   isScrolled = false;
 
   @HostListener('window:scroll', [])
+  @HostListener('window:resize', [])
   onWindowScroll() {
-    if (window.scrollY > 80) {
+    if (window.scrollY > 80 || window.innerWidth <= 991.9) {
       this.isScrolled = true;
     } else {
       this.isScrolled = false;
