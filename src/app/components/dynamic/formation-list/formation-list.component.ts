5f852ed6946be0e1e0b780aa2ea496ec2fdcbd2b
MeDjb10 <131858818+MeDjb10@users.noreply.github.com>
2023-11-17 22:23:53 +0100

    taleb rzin

--- src/app/components/dynamic/formation-list/formation-list.component.ts
+++ src/app/components/dynamic/formation-list/formation-list.component.ts
@@ -1,27 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Formation } from 'src/app/classes/formation';
 import { FormationService } from 'src/app/services/formation.service';
 
 @Component({
   selector: 'app-formation-list',
   templateUrl: './formation-list.component.html',
-  styleUrls: ['./formation-list.component.css']
+  styleUrls: ['./formation-list.component.css'],
 })
-export class FormationListComponent implements OnInit{
+export class FormationListComponent implements OnInit {
   selectedTraining: any | null = null;
   ngOnInit(): void {
-    
-      this.formatioService.getFormation().subscribe((data)=>{
-        console.log(data);
-        this.lesFormations=data
-      });
-    
+    this.formatioService.getFormation().subscribe((data) => {
+      console.log(data);
+      this.lesFormations = data;
+    });
   }
 
-  lesFormations: Formation[]=[];
-  constructor(private formatioService:FormationService, ){}
-
-  
+  lesFormations: Formation[] = [];
+  constructor(
+    private formatioService: FormationService,
+    private router: Router
+  ) {}
 
   showDetails(training: any): void {
     this.selectedTraining = training;
@@ -30,5 +30,10 @@ export class FormationListComponent implements OnInit{
   clearDetails(): void {
     this.selectedTraining = null;
   }
-  
+
+  onClick(id: number) {
+    // Naviguer vers la nouvelle page lorsque la div est cliquée
+    this.router.navigate(['/formationList/'+id]);
+    
+  }
 }
