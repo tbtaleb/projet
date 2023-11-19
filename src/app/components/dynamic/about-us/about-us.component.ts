c74b2feece04f592392dc776dac584b229653fef
MeDjb10 <131858818+MeDjb10@users.noreply.github.com>
2023-11-19 13:33:32 +0100

    chanegd the slides

--- src/app/components/dynamic/about-us/about-us.component.ts
+++ src/app/components/dynamic/about-us/about-us.component.ts
@@ -14,21 +14,22 @@ export class AboutUsComponent implements OnInit {
   ngOnInit(): void {
     this.slides[0] = {
       id: 0,
-      src: "../../../../assets/abouUsBg.png",
+      src: "../../../../assets/animus.png",
       title: 'First slide',
       subtitle: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
     };
     this.slides[1] = {
       id: 1,
-      src: "../../../../landing page.png",
+      src: '../../../../assets/animus.png',
       title: 'Second slide',
-      subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+      subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     };
     this.slides[2] = {
       id: 2,
-      src: "../../../../assets/abouUsBg.png",
+      src: '../../../../assets/animus.png',
       title: 'Third slide',
-      subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
+      subtitle:
+        'Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
     };
   }
 }
