8470628bbc6f88c2e0b1cd1c4cba6196d78417fd
tbtaleb* <tbfeeber@gmail.com>
2023-11-22 23:01:40 +0100

    search bl esm ta3 trainings w kol

--- src/app/components/dynamic/formation-list/formation-list.component.ts
+++ src/app/components/dynamic/formation-list/formation-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Formation } from 'src/app/classes/formation';
 import { FormationService } from 'src/app/services/formation.service';
@@ -9,18 +10,35 @@ import { FormationService } from 'src/app/services/formation.service';
   styleUrls: ['./formation-list.component.css'],
 })
 export class FormationListComponent implements OnInit {
+  
   selectedTraining: any | null = null;
-  ngOnInit(): void {
-    this.formatioService.getFormation().subscribe((data) => {
-      this.lesFormations = data;
-    });
-  }
-
   lesFormations: Formation[] = [];
+  searchValue='';
+  typeValue ='';
+
   constructor(
     private formatioService: FormationService,
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder,
   ) {}
+  
+  searchForm = this.fb.nonNullable.group({
+    searchValue:'',
+    typeValue:'',
+  });
+
+  ngOnInit(): void {
+    this.fetchData();
+    
+  }
+
+  fetchData(): void {
+    this.formatioService.getFormation(this.searchValue, this.typeValue).subscribe((data) => {
+        this.lesFormations = data;
+    });
+}
+
+  
 
   showDetails(training: any): void {
     this.selectedTraining = training;
@@ -34,5 +52,11 @@ export class FormationListComponent implements OnInit {
     // Naviguer vers la nouvelle page lorsque la div est cliquée
     this.router.navigate(['/formationList/'+id]);
     
+  }
+
+  onSearchSubmit():void{
+    this.typeValue = this.searchForm.value.typeValue ?? '';
+    this.searchValue = this.searchForm.value.searchValue ?? '';
+    this.fetchData();
   }
 }
